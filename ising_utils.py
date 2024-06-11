2ee88501e5740bcab478889c2c37dbf8e0dd0fe1
the-ninth-wave <34815796+the-ninth-wave@users.noreply.github.com>
2024-06-10 22:51:35 -0400

    update SW plotting

--- ising_utils.py
+++ ising_utils.py
@@ -241,10 +241,14 @@ def plot_lattice_with_bonds(lattice_snapshots, bond_snapshots, step):
     spins = lattice.flatten()
     
     colors = np.where(spins == 1, 'black', 'white')
-    ax.scatter(x, y, c=colors, s=(300 / np.sqrt(N)), edgecolors='k', zorder=2) 
+    ax.scatter(x, y, c=colors, s=(400 / N), edgecolors='k', zorder=2) 
     
     ax.set_xlim(-0.5, N + .5)
     ax.set_ylim(-0.5, N + .5)
     ax.set_aspect('equal')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
     plt.gca().invert_yaxis()  # Invert y-axis to match the imshow orientation
     plt.show()
